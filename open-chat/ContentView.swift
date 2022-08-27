8ba86ca25ad8cc38f08d2c5d1e45543db6b1ac16
tourssliver <theobaldnartha@gmail.com>
2022-08-27 14:38:26 +0800

    Initial Commit

---
+++ open-chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  open-chat
+//
+//  Created by 马翔 on 2022/8/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ open-chat/open_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  open_chatApp.swift
+//  open-chat
+//
+//  Created by 马翔 on 2022/8/27.
+//
+
+import SwiftUI
+
+@main
+struct open_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
