eafba0f9f9e5b760a6a5f80d248984caf59e34e3
maxiang <maxiang@haoshuo.com>
2022-09-08 15:52:13 +0800

    feat: add login view

--- open-chat/ui/login/LoginView.swift
+++ open-chat/ui/login/LoginView.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 struct LoginView: View {
     @EnvironmentObject var RootVM: RootViewModal
+    @State private var value = ""
+
+    init() {
+        UITableView.appearance().backgroundColor = UIColor(named: "primaryColor")
+    }
+
     var body: some View {
-        Button("Butto2n") {
-            RootVM.userFlow = .home
+        VStack {
+            Form {
+                Section(header: Text("First Name")) {
+                    TextField("First", text: $value).background(Color("tertiaryColor")).cornerRadius(3)
+                }
+
+                Section(header: Text("Last Name")) {
+                    TextField("Last", text: $value).background(Color("tertiaryColor")).cornerRadius(3)
+                }
+
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    RootVM.userFlow = .home
+                }
+            }
         }
     }
 }
