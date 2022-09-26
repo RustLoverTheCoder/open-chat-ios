bd704f36705063579bf0f3956edab1328491fc33
maxiang <maxiang@haoshuo.com>
2022-09-26 15:48:36 +0800

    feat: add rlottie width

--- open-chat/ui/lottiePlayer/StickerView.swift
+++ open-chat/ui/lottiePlayer/StickerView.swift
@@ -8,9 +8,11 @@ import SDWebImageLottieCoder
 
 struct StickerView: UIViewRepresentable {
     let lottieURL: URL
-    let imageView: UIImageView = UIImageView()
+    let imageView: SDAnimatedImageView = SDAnimatedImageView()
+    let pixelSize = CGSize(width: .tgStickerWidth, height: .tgStickerWidth)
     func makeUIView(context: Context) -> some UIView {
-        imageView.sd_setImage(with: lottieURL)
+//        imageView.sd_setImage(with: lottieURL)
+        imageView.sd_setImage(with: lottieURL, placeholderImage: nil, options: [],context: [SDWebImageContextOption.imageThumbnailPixelSize : pixelSize])
         return imageView
     }
 
